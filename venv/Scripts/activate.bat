db48916c7e2f69182836d2134529934f3ecddf62
Jiten <jitenkalal321@gmail.com>
2026-07-15 00:02:26 +0530

    added gitignore, agent.py, main.py and etc

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Jiten\OneDrive\Desktop\python practice\qanda\venv
+set VIRTUAL_ENV=C:\Users\Jiten\OneDrive\Desktop\python practice\qanda\Q-A-ai-agent\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
